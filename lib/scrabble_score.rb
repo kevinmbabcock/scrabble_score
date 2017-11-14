bf98ce4359490899d33a5e441478aa108fb3e2a8
Kevin Babcock and Qianqian Hu <qianqian@>
2017-11-14 09:41:14 -0800

    give values to f k j and q

--- lib/scrabble_score.rb
+++ lib/scrabble_score.rb
@@ -6,5 +6,20 @@ def scrabble_score(str)
   if (str == "d" || str == "D")
     scrabble.store(str, 2)
   end
+  if (str == "b" || str == "B")
+    scrabble.store(str, 3)
+  end
+  if (str == "f" || str == "F")
+    scrabble.store(str, 4)
+  end
+  if (str == "k" || str == "K")
+    scrabble.store(str, 5)
+  end
+  if (str == "j" || str == "J")
+    scrabble.store(str, 8)
+  end
+  if (str == "q" || str == "Q")
+    scrabble.store(str, 10)
+  end
   scrabble.fetch(str)
 end
--- spec/scrabble_score_spec.rb
+++ spec/scrabble_score_spec.rb
@@ -8,7 +8,27 @@ describe('scrabble_score') do
     expect(scrabble_score("a")).to(eq(1))
   end
 
-  it("add d's have a value of 2") do
+  it("all d's have a value of 2") do
     expect(scrabble_score("d")).to(eq(2))
+  end
+
+  it("all b's have a value of 3") do
+    expect(scrabble_score("b")).to(eq(3))
+  end
+
+  it("all f's have a value of 4") do
+    expect(scrabble_score("f")).to(eq(4))
+  end
+
+  it("all k's have a value of 5") do
+    expect(scrabble_score("k")).to(eq(5))
+  end
+
+  it("all j's have a value of 8") do
+    expect(scrabble_score("j")).to(eq(8))
+  end
+
+  it("all q's have a value of 10") do
+    expect(scrabble_score("q")).to(eq(10))
   end
 end
